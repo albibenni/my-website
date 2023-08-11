fbabc7a4857b0f835dfb382d189dbccd2a224fe1
Alberto Benatti <albi.benni8@gmail.com>
2023-08-11 15:49:12 +0200

    ts refactor components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import { Hero, Navbar } from '@/components';
+
 export default function Home() {
   return (
     <div className="relative z-0 bg-primary">
-      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center"></div>
-      Ciao
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
+      </div>
     </div>
   );
 }
