c30303e8a3846e3e1140d7060fa2cf529ebb0c46
Alberto Benatti <albi.benni8@gmail.com>
2023-08-11 11:55:19 +0200

    styles files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-import type { Config } from 'tailwindcss'
+/** @type {import('tailwindcss').Config} */
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -6,15 +7,31 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  mode: 'jit',
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'hero-pattern': "url('/src/assets/herobg.png')",
+        // 'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        // 'gradient-conic':
+        //   'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        primary: '#050816',
+        secondary: '#aaa6c3',
+        tertiary: '#151030',
+        'black-100': '#100d25',
+        'black-200': '#090325',
+        'white-100': '#f3f3f3',
+      },
+      boxShadow: {
+        card: '0px 35px 120px -15px #211e35',
+      },
+      screens: {
+        xs: '450px',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
