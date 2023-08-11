640b34ec3915a99240fd35400396488e0fd5c2c3
Alberto Benatti <albi.benni8@gmail.com>
2023-08-11 15:51:19 +0200

    main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,14 @@
-import { Hero, Navbar } from '@/components';
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  StarsCanvas,
+  Tech,
+  Works,
+} from '@/components';
 
 export default function Home() {
   return (
@@ -6,6 +16,15 @@ export default function Home() {
       <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
         <Navbar />
         <Hero />
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div className="relative z-0">
+        <Contact />
+        <StarsCanvas />
       </div>
     </div>
   );
