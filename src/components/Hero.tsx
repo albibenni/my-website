507062fccace67d2d79428f69be4d9220944a40d
Alberto Benatti <albi.benni8@gmail.com>
2023-08-12 15:59:47 +0200

    fixed canvas position

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -24,8 +24,8 @@ export default function Hero() {
             enjoy trying new tech.
           </p>
         </div>
-        <ComputerCanvas />
       </div>
+      <ComputerCanvas />
     </section>
   );
 }
