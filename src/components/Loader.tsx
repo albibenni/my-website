0ad3c80d317e1602b3f75249ba9c2b7f1f35386f
Alberto Benatti <albi.benni8@gmail.com>
2023-08-12 17:40:54 +0200

    loader in %

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,6 +1,22 @@
 'use client';
 import React from 'react';
+import { Html, useProgress } from '@react-three/drei';
 
 export default function Loader() {
-  return <>Loader</>;
+  const { progress } = useProgress();
+  return (
+    <Html>
+      <span className="canvas-loader"></span>
+      <p
+        style={{
+          fontSize: 14,
+          color: '#f1f1f1',
+          fontWeight: 800,
+          marginTop: 40,
+        }}
+      >
+        {progress.toFixed(2)}%
+      </p>
+    </Html>
+  );
 }
