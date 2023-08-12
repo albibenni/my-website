9d4b2837da6d4f53e359d0eea89a6c8cffa10a0b
Alberto Benatti <albi.benni8@gmail.com>
2023-08-12 18:23:21 +0200

    about page wip

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,39 @@
+'use client';
 import React from 'react';
+import { motion } from 'framer-motion';
+import { styles } from '@/app/styles';
+import { fadeIn, textVariant } from '@/utils/motion';
+import { services } from '@/constants';
+import ServiceCard from '@/components/ServiceCard';
 
 export default function About() {
-  return <div>About</div>;
+  const blockOfText =
+    "I'm a software engineer with experience in TypeScript,\n" +
+    '          JavaScript and Java and expertise in frameworks like React, Node.js, and\n' +
+    "          Spring. I'm a quick learner and I enjoy learning new stuff.\n" +
+    "Let's work together to bring your ideas to life!";
+  return (
+    <>
+      <motion.div variants={textVariant(0)}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <p className={styles.sectionHeadText}>Overview </p>
+      </motion.div>
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        {blockOfText}
+      </motion.p>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard
+            key={service.title}
+            title={service.title}
+            index={index}
+            icon={service.icon}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
