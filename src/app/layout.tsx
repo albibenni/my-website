7585ee25f0040fd30ca14dee7fbcc2adc9bf300d
Alberto Benatti <albi.benni8@gmail.com>
2023-08-12 15:09:37 +0200

    fix layour and homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import './globals.css';
 import type { Metadata } from 'next';
+import React from 'react';
 import { Inter } from 'next/font/google';
-import { Navbar } from '@/components';
+import Navbar from '@/components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
-import {
-  About,
-  Contact,
-  Experience,
-  Feedbacks,
-  Hero,
-  StarsCanvas,
-  Tech,
-  Works,
-} from '@/components';
+import React from 'react';
+import Hero from '@/components/Hero';
+import About from '@/components/About';
+import Experience from '@/components/Experience';
+import Tech from '@/components/Tech';
+import Works from '@/components/Works';
+import Feedbacks from '@/components/Feedbacks';
+import Contact from '@/components/Contact';
+import StarsCanvas from '@/components/canvas/StarsCanvas';
 
 export default function Home() {
   return (
