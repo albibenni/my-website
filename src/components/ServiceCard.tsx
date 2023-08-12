40880ea8b916636c51e97fae201cde759cff11d9
Alberto Benatti <albi.benni8@gmail.com>
2023-08-12 18:23:41 +0200

    card

---
+++ src/components/ServiceCard.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { fadeIn } from '@/utils/motion';
+import { Tilt } from 'react-tilt';
+import Image, { StaticImageData } from 'next/image';
+//    "react-tilt": "^0.1.4",
+
+interface ServiceCardProps {
+  index: number;
+  title: string;
+  icon: StaticImageData;
+}
+export default function ServiceCard({ index, title, icon }: ServiceCardProps) {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        {/*<div*/}
+        {/*  options={{*/}
+        {/*    max: 45,*/}
+        {/*    scale: 1,*/}
+        {/*    speed: 450,*/}
+        {/*  }}*/}
+        {/*  className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"*/}
+        {/*>*/}
+        <Image
+          src={icon}
+          alt="web-development"
+          className="w-16 h-16 object-contain"
+        />
+
+        <h3 className="text-white text-[20px] font-bold text-center">
+          {title}
+        </h3>
+        {/*</div>*/}
+      </motion.div>
+    </Tilt>
+  );
+}
