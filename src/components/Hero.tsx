2df59de8f08fd586f8202021f587e63e5986ac82
Alberto Benatti <albi.benni8@gmail.com>
2023-08-12 17:56:17 +0200

    hero about

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,6 @@
+'use client';
 import React from 'react';
+import { motion } from 'framer-motion';
 import { styles } from '@/app/styles';
 import ComputerCanvas from '@/components/canvas/ComputerCanvas';
 
@@ -26,6 +28,24 @@ export default function Hero() {
         </div>
       </div>
       <ComputerCanvas />
+      {/*TODO xs:bottom-8 bottom-16 weird outcome -- tailwind css*/}
+      <div className="absolute xs:bottom-8 bottom-16 w-full flex justify-center items-center ">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop',
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 }
