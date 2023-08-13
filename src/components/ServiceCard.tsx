46d1ef880ec69e8e7e27ffe173617273206e5245
Alberto Benatti <albi.benni8@gmail.com>
2023-08-13 08:43:33 +0200

    card

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -17,24 +17,27 @@ export default function ServiceCard({ index, title, icon }: ServiceCardProps) {
         variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
         className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
       >
-        {/*<div*/}
-        {/*  options={{*/}
-        {/*    max: 45,*/}
-        {/*    scale: 1,*/}
-        {/*    speed: 450,*/}
-        {/*  }}*/}
-        {/*  className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"*/}
-        {/*>*/}
-        <Image
-          src={icon}
-          alt="web-development"
-          className="w-16 h-16 object-contain"
-        />
+        <div
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          // eslint-disable-next-line react/no-unknown-property
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <Image
+            src={icon}
+            alt="web-development"
+            className="w-16 h-16 object-contain"
+          />
 
-        <h3 className="text-white text-[20px] font-bold text-center">
-          {title}
-        </h3>
-        {/*</div>*/}
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
       </motion.div>
     </Tilt>
   );
