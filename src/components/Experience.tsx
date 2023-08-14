f7cb24d13883e3f8867dfd6773c597b0e41ceee7
Alberto Benatti <albi.benni8@gmail.com>
2023-08-14 09:49:51 +0200

    experience tab

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -2,8 +2,11 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { textVariant } from '@/utils/motion';
+import { VerticalTimeline } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import { styles } from '@/app/styles';
+import ExperienceCard from '@/components/ExperienceCard';
+import { experiences } from '@/constants/recap';
 
 export default function Experience() {
   const subText = "What I've done so far";
@@ -16,6 +19,16 @@ export default function Experience() {
         <p className={styles.sectionSubText}>{subText}</p>
         <h2 className={styles.sectionHeadText}>Work experience</h2>
       </motion.div>
+      <div className="mt-20 flex flex-col">
+        <VerticalTimeline>
+          {experiences.map((experience, index) => (
+            <ExperienceCard
+              key={`experience-${index}`}
+              experience={experience}
+            />
+          ))}
+        </VerticalTimeline>
+      </div>
     </>
   );
 }
