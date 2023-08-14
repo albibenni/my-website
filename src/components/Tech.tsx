efbfe7fb66cca3f29f9af0245d54c16a74a454ee
Alberto Benatti <albi.benni8@gmail.com>
2023-08-14 15:26:59 +0200

    added tech part

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { technologies } from '@/constants/recap';
+import BallCanvas from '@/components/canvas/BallCanvas';
 
 export default function Tech() {
-  return <div>Tech</div>;
+  return (
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((tech, index) => (
+        <div key={index} className="w-28 h-28">
+          <BallCanvas tech={tech} />
+        </div>
+      ))}
+    </div>
+  );
 }
