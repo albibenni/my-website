d374e9fdfcf4b34985a249e69196b4d70bb29837
Alberto Benatti <albi.benni8@gmail.com>
2023-08-14 18:40:05 +0200

    span with id technologies in case anchor is needed

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -1,15 +1,21 @@
+'use client';
 import React from 'react';
 import { technologies } from '@/constants/recap';
 import BallCanvas from '@/components/canvas/BallCanvas';
 
 export default function Tech() {
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((tech, index) => (
-        <div key={index} className="w-28 h-28">
-          <BallCanvas tech={tech} />
-        </div>
-      ))}
-    </div>
+    <>
+      <span className="hash-span" id={'technologies'}>
+        &nbsp;
+      </span>
+      <div className="flex flex-row flex-wrap justify-center gap-10">
+        {technologies.map((tech, index) => (
+          <div key={index} className="w-28 h-28">
+            <BallCanvas tech={tech} />
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
