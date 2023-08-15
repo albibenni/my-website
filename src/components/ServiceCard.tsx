9778db21f9b90ff7642c423336a2fb5e2420a737
Alberto Benatti <albi.benni8@gmail.com>
2023-08-15 16:29:19 +0200

    fixed tilt component position

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -1,40 +1,37 @@
+'use client';
 import React from 'react';
 import { motion } from 'framer-motion';
 import { fadeIn } from '@/utils/motion';
 import { Tilt } from 'react-tilt';
-import Image, { StaticImageData } from 'next/image';
+import Image from 'next/image';
+import { Service } from '@/constants/recap';
 
 interface ServiceCardProps {
   index: number;
-  title: string;
-  icon: StaticImageData;
+  service: Service;
 }
-export default function ServiceCard({ index, title, icon }: ServiceCardProps) {
+export default function ServiceCard({ index, service }: ServiceCardProps) {
   return (
-    <Tilt className="xs:w-[250px] w-full">
+    <Tilt
+      options={{
+        max: 45,
+        scale: 1,
+        speed: 450,
+      }}
+      className="xs:w-[250px] w-full"
+    >
       <motion.div
         variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
         className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
       >
-        <div
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-ignore
-          // eslint-disable-next-line react/no-unknown-property
-          options={{
-            max: 45,
-            scale: 1,
-            speed: 450,
-          }}
-          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
-        >
+        <div className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
           <Image
-            src={icon}
+            src={service.icon}
             alt="web-development"
             className="w-16 h-16 object-contain"
           />
-
           <h3 className="text-white text-[20px] font-bold text-center">
-            {title}
+            {service.title}
           </h3>
         </div>
       </motion.div>
