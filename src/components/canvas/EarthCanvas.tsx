1ae3596d1814065c195135f71503f3440a0c4ca5
Alberto Benatti <albi.benni8@gmail.com>
2023-08-16 11:43:21 +0200

    Earth canvas

---
+++ src/components/canvas/EarthCanvas.tsx
@@ -0,0 +1,34 @@
+'use client';
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload } from '@react-three/drei';
+import Earth from '@/components/canvas/Earth';
+import CanvasLoader from '../Loader';
+
+export default function EarthCanvas() {
+  return (
+    <Canvas
+      shadows
+      frameloop="demand"
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Earth />
+        <Preload all />
+      </Suspense>
+    </Canvas>
+  );
+}
