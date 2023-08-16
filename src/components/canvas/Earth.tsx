436ac909dff6bcb719d35964b4c2b393ea050cbc
Alberto Benatti <albi.benni8@gmail.com>
2023-08-16 11:43:15 +0200

    Earth base

--- src/components/canvas/Earth.tsx
+++ src/components/canvas/Earth.tsx
@@ -1,6 +1,12 @@
 'use client';
 import React from 'react';
+import { useGLTF } from '@react-three/drei';
 
 export default function Earth() {
-  return <div>Earth</div>;
+  const earth = useGLTF('./planet/scene.gltf');
+
+  return (
+    // eslint-disable-next-line react/no-unknown-property
+    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+  );
 }
