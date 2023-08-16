a06f372483640621a94f0e704d7054aa2f058ed1
Alberto Benatti <albi.benni8@gmail.com>
2023-08-16 14:45:32 +0200

    starts canvas

--- src/components/canvas/StarsCanvas.tsx
+++ src/components/canvas/StarsCanvas.tsx
@@ -1,6 +1,19 @@
 'use client';
-import React from 'react';
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { Preload } from '@react-three/drei';
+import Stars from '@/components/canvas/Stars';
 
 export default function StarsCanvas() {
-  return <div>Stars</div>;
+  return (
+    <div className="w-full h-full absolute inset-0 z-[-1]">
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+
+        <Preload all />
+      </Canvas>
+    </div>
+  );
 }
