840fe91532e4e80baae9e5351e40ece9c2d80f36
Alberto Benatti <albi.benni8@gmail.com>
2023-08-16 20:40:36 +0200

    env for emailjs service

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        EMAILJS_SERVICE_ID:'service_14ahgac',
+        EMAILJS_TEMPLATE_ID:'template_rrzhk0l',
+        EMAILJS_PUBLIC_KEY:'EvWfF5ddosV2NBBY3',
+    }
+}
 
 module.exports = nextConfig
