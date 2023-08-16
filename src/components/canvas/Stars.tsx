14d70ef99d71f7dc172b2367f8971f9a35ac00df
Alberto Benatti <albi.benni8@gmail.com>
2023-08-16 15:00:28 +0200

    stars done

--- src/components/canvas/Stars.tsx
+++ src/components/canvas/Stars.tsx
@@ -1,6 +1,43 @@
 'use client';
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { PointMaterial, Points } from '@react-three/drei';
+import { random } from 'maath';
+import { useFrame } from '@react-three/fiber';
 
 export default function Stars() {
-  return <div>Stars</div>;
+  const ref = useRef(null);
+  const [sphere] = useState(() =>
+    random.inSphere(new Float32Array(5000), { radius: 1.2 })
+  );
+  useFrame((state, delta) => {
+    if (ref !== null) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      ref.current.rotation.x -= delta / 10;
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      ref.current.rotation.y -= delta / 15;
+    }
+  });
+
+  return (
+    <group>
+      <Points
+        ref={ref}
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        positions={sphere}
+        stride={3}
+        frustumCulled
+      >
+        <PointMaterial
+          transparent
+          color="#f272c8"
+          size={0.002}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
 }
