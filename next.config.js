7d62b259e4c485426a5a9dc0108590d4ffbab2c2
Alberto Benatti <albi.benni8@gmail.com>
2023-08-17 15:12:03 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: 'dist',
     env: {
         EMAILJS_SERVICE_ID:'service_14ahgac',
         EMAILJS_TEMPLATE_ID:'template_rrzhk0l',
